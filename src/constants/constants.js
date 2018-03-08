8002297f8bbf7ea227863ca636179735c8b8c2f1
AndjDavis <andi.idavis@gmail.com>
2018-03-08 16:14:53 -0700

    added constants/constants.js with basic coloring added, added dummy data to data/data.js

---
+++ src/constants/constants.js
@@ -0,0 +1,12 @@
+export const COLORS = {
+  Gray: {
+    Black_Russian: '#0A0A0B',
+    Bright_Gray: '#5A5B60'
+  },
+  Red: {
+    Fire_Engine_Red: '#D81120'
+  },
+  White: {
+    White: '#FFFFFF',
+  }
+}
